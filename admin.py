eee5906eb85f9aecd057f5851ef4426dbb38bee7
Richard Smiley <rsmiley@gmail.com>
2009-09-13 20:25:15 -0400

    models for debate, entry, comment and admin.py

---
+++ admin.py
@@ -0,0 +1,6 @@
+from models import Debate, Entry, Comment
+from django.contrib import admin
+
+admin.site.register(Debate)
+admin.site.register(Entry)
+admin.site.register(Comment)
--- models.py
+++ models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+POINT_CHOICES = (
+    (-1, -1),
+    (0, 0),
+    (1, 1),
+    )
+
+class Debate(models.Model):
+  title = models.CharField(max_length=200)
+  pub_date = models.DateTimeField() # default to now
+  instigator = models.ForeignKey(User)
+
+class Entry(models.Model):
+  debate = models.ForeignKey(Debate)
+  text = models.TextField()
+  author = models.ForeignKey(User)
+  pub_date = models.DateTimeField() # default to now
+
+class Comment(models.Model):
+  author = models.ForeignKey(User)
+  text = models.TextField()
+  parentEntry = models.ForeignKey(Entry)
+  parentComment = models.ForeignKey('self')
+  pub_date = models.DateTimeField() # default to now
+  parentPoints = models.SmallIntegerField(choices = POINT_CHOICES)

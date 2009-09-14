ba7cdc6c42147f453fc26e8946560bf90b4fb7d6
Richard Smiley <rsmiley@gmail.com>
2009-09-13 21:03:13 -0400

    front page for site lists all debates

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    (r'^', include('debately-site.debately.urls')),
+)
--- views.py
+++ views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from models import Debate
+from django.shortcuts import render_to_response
+
+def index(req):
+  # list all debates by pubdate
+  debates = Debate.objects.all().order_by('-pub_date')
+  return render_to_response('index.html', {'debates': debates})
+

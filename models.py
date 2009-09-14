962470c574fbf5f27dc838325704f69754c9ec3a
Richard Smiley <rsmiley@gmail.com>
2009-09-13 20:41:35 -0400

    auto_add_now for datetime fields

--- models.py
+++ models.py
@@ -8,21 +8,21 @@ POINT_CHOICES = (
     )
 
 class Debate(models.Model):
-  title = models.CharField(max_length=200)
+  title = models.CharField(max_length = 200)
   summary = models.TextField() # not for the first entry!  just a summary, please
-  pub_date = models.DateTimeField() # default to now
+  pub_date = models.DateTimeField(auto_now_add = True)
   instigator = models.ForeignKey(User)
 
 class Entry(models.Model):
   debate = models.ForeignKey(Debate)
   text = models.TextField()
   author = models.ForeignKey(User)
-  pub_date = models.DateTimeField() # default to now
+  pub_date = models.DateTimeField(auto_now_add = True)
 
 class Comment(models.Model):
   author = models.ForeignKey(User)
   text = models.TextField()
   parentEntry = models.ForeignKey(Entry)
   parentComment = models.ForeignKey('self')
-  pub_date = models.DateTimeField() # default to now
+  pub_date = models.DateTimeField(auto_now_add = True)
   parentPoints = models.SmallIntegerField(choices = POINT_CHOICES)

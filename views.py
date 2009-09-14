5d117a3be550015bede7e2a8d5aa7cf8694782d7
Richard Smiley <rsmiley@gmail.com>
2009-09-13 23:31:30 -0400

    distinguish between debate points and comment points

--- views.py
+++ views.py
@@ -7,15 +7,17 @@ def index(req):
   debates = Debate.objects.all().order_by('-pub_date')
   return render_to_response('index.html', {'debates': debates})
 
-def getPoints(debate, user):
-  return Comment.objects.filter(parentEntry__debate = debate, parentEntry__author = user).aggregate(Sum('parentPoints'))['parentPoints__sum']
+def getDebatePoints(debate, user):
+  return Comment.objects.filter(parentEntry__debate = debate, parentEntry__author = user, parentComment = None).aggregate(Sum('parentPoints'))['parentPoints__sum']
 
+def getCommentPoints(debate, user):
+  raise Exception("not implemented yet")
 
 def debate(req, id):
   debate = Debate.objects.get(id = id)
   c = { "debate": debate,
-      "instigatorpoints": getPoints(debate, debate.instigator),
-      "challengerpoints": getPoints(debate, debate.challenger),
+      "instigatorpoints": getDebatePoints(debate, debate.instigator),
+      "challengerpoints": getDebatePoints(debate, debate.challenger),
       "entries": Entry.objects.filter(debate = debate)
       }
   return render_to_response('debate.html', c)

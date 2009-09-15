c16b70993888c543feb724c7b771e265ca1cd660
carl <carl@komputermaschine.com>
2009-09-15 12:52:04 -0700

    add basic css for layout and static file serving for dev server

--- urls.py
+++ urls.py
@@ -1,7 +1,24 @@
+import os
+
+from django.conf import settings
 from django.conf.urls.defaults import *
+
+DEBATELY_STATIC_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'static')
 
 urlpatterns = patterns('debately.views',
     (r'^debates/(\d+)$', 'debate'),
     (r'^users/(.*)', 'userpage'),
     (r'^$', 'index'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',    
+    # the following is for service static media in the development
+    # environ. Should not be used in production 
+    # see http://docs.djangoproject.com/en/dev/howto/static-files/
+    # for details
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': DEBATELY_STATIC_PATH}),
+    )
+

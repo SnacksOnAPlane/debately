48e5f984d1d4f15b9fb0aad78a40588192c59f88
carl <carl@komputermaschine.com>
2009-09-15 15:47:14 -0700

    fix formatting/naming to better match PEP8 guidelines

--- models.py
+++ models.py
@@ -1,53 +1,59 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 POINT_CHOICES = (
     (-1, -1),
     (0, 0),
     (1, 1),
-    )
+)
+
 
 class Debate(models.Model):
-  title = models.CharField(max_length = 200)
-  summary = models.TextField() # not for the first entry!  just a summary, please
-  pub_date = models.DateTimeField(auto_now_add = True)
-  instigator = models.ForeignKey(User, related_name = "instigator")
-  challenger = models.ForeignKey(User, related_name = "challenger")
+    title = models.CharField(max_length = 200)
+    summary = models.TextField() #not for the first entry! just a summary, please
+    pub_date = models.DateTimeField(auto_now_add = True)
+    instigator = models.ForeignKey(User, related_name = "instigator")
+    challenger = models.ForeignKey(User, related_name = "challenger")
+
 
 class Entry(models.Model):
-  debate = models.ForeignKey(Debate)
-  text = models.TextField()
-  author = models.ForeignKey(User)
-  pub_date = models.DateTimeField(auto_now_add = True)
+    debate = models.ForeignKey(Debate)
+    text = models.TextField()
+    author = models.ForeignKey(User)
+    pub_date = models.DateTimeField(auto_now_add = True)
+
 
 class Comment(models.Model):
-  author = models.ForeignKey(User)
-  text = models.TextField()
-  parentEntry = models.ForeignKey(Entry)
-  parentComment = models.ForeignKey('self', null = True, blank = True)
-  pub_date = models.DateTimeField(auto_now_add = True)
-  parentPoints = models.SmallIntegerField(choices = POINT_CHOICES, default = 0)
+    author = models.ForeignKey(User)
+    text = models.TextField()
+    parent_entry = models.ForeignKey(Entry)
+    parent_comment = models.ForeignKey('self', null = True, 
+                                      blank = True)
+    pub_date = models.DateTimeField(auto_now_add = True)
+    parent_points = models.SmallIntegerField(choices = POINT_CHOICES, 
+                                            default = 0)
 
 
 class UserProfile(models.Model):
-  """
-  A debate profile for users containing debate site details for each user.
-  To get a user profile, call use the get_for_user(user_name) class method
-  """
-  user = models.ForeignKey(User)
-  join_date = models.DateTimeField(auto_now_add=True)
+    """
+    A debate profile for users containing debate site details for each user.
+    To get a user profile, call use the get_for_user(user_name) class method
+    """
+    user = models.ForeignKey(User)
+    join_date = models.DateTimeField(auto_now_add=True)
   
-  @classmethod
-  def get_profile_for_user(cls, profile_user):
-    """
-    Get debate user profile for given user.
-    Creates new UserProfile for user if a user profile does not already exist.
-    """
-    try:
-      profile = UserProfile.objects.filter(user=profile_user)[0]
-    except IndexError:
-      profile = UserProfile(user=profile_user)
-      profile.save()
+    @classmethod
+    def get_profile_for_user(cls, profile_user):
+        """
+        Get debate user profile for given user.
+        Creates new UserProfile for user if a user profile does not already exist.
+        """
+        try:
+            profile = UserProfile.objects.filter(user=profile_user)[0]
+        except IndexError:
+            profile = UserProfile(user=profile_user)
+            profile.save()
 
-    return profile
+        return profile
     
--- views.py
+++ views.py
@@ -1,39 +1,42 @@
-from django.db.models import Sum
 from django.contrib.auth.models import User
+from django.db.models import Sum
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
 from models import Debate, Entry, Comment, UserProfile
 
 def index(req):
-  # list all debates by pubdate
-  debates = Debate.objects.all().order_by('-pub_date')
-  return render_to_response('index.html', 
-                            RequestContext(req, {'debates': debates}))
+    # list all debates by pubdate
+    debates = Debate.objects.all().order_by('-pub_date')
+    return render_to_response('index.html', 
+                              RequestContext(req, {'debates': debates}))
 
-def getDebatePoints(debate, user):
-  return Comment.objects.filter(parentEntry__debate = debate, parentEntry__author = user, parentComment = None).aggregate(Sum('parentPoints'))['parentPoints__sum']
+def get_debate_points(debate, user):
+    return Comment.objects.filter(parent_entry__debate = debate, 
+                                  parent_entry__author = user, 
+                                  parent_comment = None).aggregate(
+                                      Sum('parent_points'))['parent_points__sum']
 
-def getCommentPoints(debate, user):
-  raise Exception("not implemented yet")
+def get_comment_points(debate, user):
+    raise Exception("not implemented yet")
 
 def debate(req, id):
-  debate = Debate.objects.get(id = id)
-  c = { "debate": debate,
-      "instigatorpoints": getDebatePoints(debate, debate.instigator),
-      "challengerpoints": getDebatePoints(debate, debate.challenger),
-      "entries": Entry.objects.filter(debate = debate)
-      }
-  return render_to_response('debate.html', RequestContext(req, c))
+    debate = Debate.objects.get(id = id)
+    c = { "debate": debate,
+          "instigatorpoints": get_debate_points(debate, debate.instigator),
+          "challengerpoints": get_debate_points(debate, debate.challenger),
+          "entries": Entry.objects.filter(debate = debate)
+         }
+    return render_to_response('debate.html', RequestContext(req, c))
 
 
 def userpage(req, user_name):
-  """
-  Shows user page for valid users, returns 404 if user is not found.
-  """
-  profile_user = get_object_or_404(User, username=user_name)
-  user_profile = UserProfile.get_profile_for_user(profile_user)
-  c = { 'profile_user': profile_user,
-        'user_profile' : user_profile }
-  return render_to_response('user_profile.html', 
-                            RequestContext( req, c))
+    """
+    Shows user page for valid users, returns 404 if user is not found.
+    """
+    profile_user = get_object_or_404(User, username=user_name)
+    user_profile = UserProfile.get_profile_for_user(profile_user)
+    c = { 'profile_user': profile_user,
+          'user_profile' : user_profile }
+    return render_to_response('user_profile.html', 
+                              RequestContext( req, c))

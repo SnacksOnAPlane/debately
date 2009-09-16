4cfcde0cc0d5d453f085b3c969b86b3f48699456
carl <carl@komputermaschine.com>
2009-09-16 15:56:54 -0700

    Removing css, moved l&f base template to debately-site

--- urls.py
+++ urls.py
@@ -1,10 +1,6 @@
 import os
 
-from django.conf import settings
 from django.conf.urls.defaults import *
-
-DEBATELY_STATIC_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'static')
 
 urlpatterns = patterns('debately.views',
     (r'^debates/(\d+)$', 'debate'),
@@ -12,16 +8,3 @@ urlpatterns = patterns('debately.views',
     (r'^users/(.*)', 'userpage'),
     (r'^$', 'index'),
 )
-
-if settings.DEBUG:
-    urlpatterns += patterns('',    
-    # the following is for service static media in the development
-    # environ. Should not be used in production 
-    # see http://docs.djangoproject.com/en/dev/howto/static-files/
-    # for details
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': DEBATELY_STATIC_PATH}),
-    )
-    
-    
-

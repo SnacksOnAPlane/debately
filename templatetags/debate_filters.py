2c07c51d327d16899942b9b259d5945ce431bcf8
komputermaschine <carl@komputermaschine.com>
2009-09-17 15:17:07 -0700

    minor reformat of ugly code.

--- templatetags/debate_filters.py
+++ templatetags/debate_filters.py
@@ -4,6 +4,4 @@ register = template.Library()
 
 @register.filter("can_user_challenge")
 def can_user_challenge(debate, user):
-    if debate.can_user_challenge(user):
-        return True
-    return False
+    return debate.can_user_challenge(user)
